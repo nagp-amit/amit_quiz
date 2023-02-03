4f5e1ffbe4db20714f427775157d144bafe12ad6
Amit Kumar <amitverma1150@gmail.com>
2023-02-03 02:22:22 -0500

    tab

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -18,70 +18,74 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: DefaultAppBar(
-        title: 'Quiz App',
-        context: context,
-      ),
-      drawer: const QuizDrawer(),
-      body: BlocBuilder<CategoryCubit, AppStates>(
-        builder: (_, state) {
-          if (state is GetCategorySuccessState) {
-            return TabBarView(children: [
-              Expanded(
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        ListView.builder(
-                          itemCount: state.categories.length,
-                          physics: const ClampingScrollPhysics(),
-                          shrinkWrap: true,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(
-                                  bottom: 10, right: 10, left: 10),
-                              child: InkWell(
-                                  onTap: () => {},
-                                  child: CardItem(
-                                    category: state.categories[index],
-                                  )),
-                            );
-                          },
-                        )
-                      ],
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: DefaultAppBar(
+          title: 'Quiz App',
+          context: context,
+        ),
+        drawer: const QuizDrawer(),
+        body: BlocBuilder<CategoryCubit, AppStates>(
+          builder: (_, state) {
+            if (state is GetCategorySuccessState) {
+              return TabBarView(children: [
+                Expanded(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          ListView.builder(
+                            itemCount: state.categories.length,
+                            physics: const ClampingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(
+                                    bottom: 10, right: 10, left: 10),
+                                child: InkWell(
+                                    onTap: () => {},
+                                    child: CardItem(
+                                      category: state.categories[index],
+                                    )),
+                              );
+                            },
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                //for third tab
-                height: 400,
-                color: Colors.blue,
-              )
-            ]);
-          }
-          if (state is GetCategoryErrorState) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CircularProgressIndicator(),
-                  const SizedBox(height: 24),
-                  Text(state.error, style: const TextStyle(fontSize: 24)),
-                ],
-              ),
-            );
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
+                Container(
+                  //for third tab
+                  height: 400,
+                  color: Colors.blue,
+                )
+              ]);
+            }
+            if (state is GetCategoryErrorState) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CircularProgressIndicator(),
+                    const SizedBox(height: 24),
+                    Text(state.error, style: const TextStyle(fontSize: 24)),
+                  ],
+                ),
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
